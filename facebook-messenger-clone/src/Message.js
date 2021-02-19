403cf08515344bb30e59c350fd8d92ae2456c6e8
YB-BigSwan <swanson.stephen.h@gmail.com>
2021-02-19 17:39:58 +0200

    adds styling for message cards

--- facebook-messenger-clone/src/Message.js
+++ facebook-messenger-clone/src/Message.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
-import './Message.css'
+import './Message.css';
 
-function Message(props) {
+function Message({ message, username }) {
+
+  const isUser = username === message.username;
+
   return (
-    <Card className='message_card'>
-      <CardContent>
-        <Typography color='white' variant='h5' component='h2'>
-          {props.username}: {props.text}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className= {`message_card ${isUser && 'message_user'}`} >
+      <Card className= {isUser ? 'message_userCard' : 'message_guestCard'} >
+        <CardContent>
+          <Typography color='white' variant='h5' component='h2'>
+            {message.username}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
